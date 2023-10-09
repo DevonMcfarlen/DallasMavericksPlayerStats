99bf9fdc5879243a91a3a15c472ea7b3d3a09997
jae kim <jaehkim23@gmail.com>
2023-10-09 14:51:27 -0500

    first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-const bingSettings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=bread',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
-		'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
-	}
-};
+//const bingSettings = {
+//	async: true,
+//	crossDomain: true,
+//	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=bread',
+//	method: 'GET',
+//	headers: {
+//		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
+//		'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
+//	}
+//};
 
-$.ajax(bingSettings).done(function (response) {
-	console.log(response);
-});
+//$.ajax(bingSettings).done(function (response) {
+//	console.log(response);
+//});
 
-const nbaSettings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://api-nba-v1.p.rapidapi.com/teams',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-	}
-};
+//const nbaSettings = {
+//	async: true,
+//	crossDomain: true,
+//	url: 'https://api-nba-v1.p.rapidapi.com/teams',
+//	method: 'GET',
+//	headers: {
+//		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
+//		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+//	}
+//};
 
-$.ajax(nbaSettings).done(function (response) {
-	console.log(response);
-});
+//$.ajax(nbaSettings).done(function (response) {
+//	console.log(response);
+//});

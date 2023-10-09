bf2a3ce4e5bd4ec44e3afaa84c07a427ae0cba99
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-10-09 12:25:54 -0500

    remove misc files"

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const settings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=dallas',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
+		'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});

6c615e68f37d979ecbca5a46da077147b42a134c
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-10-09 13:01:47 -0500

    add functions to get NBA teams and player stats, and change the fetch settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,57 @@ $.ajax(bingSettings).done(function (response) {
 	console.log(response);
 });
 
-const nbaSettings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://api-nba-v1.p.rapidapi.com/teams',
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
-		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-	}
-};
+teamPlayers = [];
 
-$.ajax(nbaSettings).done(function (response) {
-	console.log(response);
-});
+
+var nbaUrl = "https://api-nba-v1.p.rapidapi.com";
+    var nbaSettings = {
+      "url": nbaUrl,
+      "method": "GET",
+      "timeout": 0,
+      "headers": {
+        "x-rapidapi-key": "09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8",
+        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+      },
+    };
+
+
+function setNBASettings(sentUrl) {
+  nbaUrl = "https://api-nba-v1.p.rapidapi.com" + sentUrl;
+  nbaSettings = {
+    "url": nbaUrl,
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "x-rapidapi-key": "09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8",
+      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
+    },
+  };
+}
+
+
+function getTeam() {
+  let toSendUrl = "/players/" + "?" + "team=8" + "&" + "season=2022";
+  setNBASettings(toSendUrl);
+
+
+  $.ajax(nbaSettings).done(function (response) {
+    console.log(response);
+    teamPlayers = response.response;
+  });
+}
+
+
+  function getPlayerStats() {
+    let toSendUrl = "/players/statistics" + "?" + "id=" + teamPlayers[3].id + "&" + "season=2022" ;
+    setNBASettings(toSendUrl);
+
+
+    $.ajax(nbaSettings).done(function (response) {
+      console.log(response);
+    });
+  }
+
+
+  getTeam();
+  setTimeout(() => {getPlayerStats();}, 1000);

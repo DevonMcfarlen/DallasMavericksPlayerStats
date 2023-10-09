ee0011b5fbe572497201ce3f4bfd75fb96f4f5e5
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-10-09 12:39:20 -0500

    add working html and js for api and semantic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const settings = {
+const bingSettings = {
 	async: true,
 	crossDomain: true,
-	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=dallas',
+	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=bread',
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
@@ -9,6 +9,21 @@ const settings = {
 	}
 };
 
-$.ajax(settings).done(function (response) {
+$.ajax(bingSettings).done(function (response) {
+	console.log(response);
+});
+
+const nbaSettings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://api-nba-v1.p.rapidapi.com/teams',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
+		'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+	}
+};
+
+$.ajax(nbaSettings).done(function (response) {
 	console.log(response);
 });

3c03e45a8dcd4d1427e8795e7bf06c9cbde2cf07
jae kim <jaehkim23@gmail.com>
2023-10-10 09:34:46 -0500

    created sample flip card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+/*
+const bingSettings = {
+	async: true,
+	crossDomain: true,
+	url: 'https://bing-image-search1.p.rapidapi.com/images/search?q=bread',
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8',
+		'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
+	}
+};
+
+$.ajax(bingSettings).done(function (response) {
+	console.log(response);
+});
+*/
+
 /*teamPlayers = [];
 
 var nbaUrl = "https://api-nba-v1.p.rapidapi.com";
